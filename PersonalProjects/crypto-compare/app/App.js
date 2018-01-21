16b9075c7cc3601760321e17ae163474add70b22
ziggysauce <dtn3423@gmail.com>
2018-01-21 01:58:24 -0500

    initial setup for crypto directory

---
+++ PersonalProjects/crypto-compare/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Initial Test</div>
+    );
+  }
+}
+
+export default App;

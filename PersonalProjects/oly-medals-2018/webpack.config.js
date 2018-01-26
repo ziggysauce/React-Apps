db03f227256cd38724e87a8a2fdb400ea21a7f21
ziggysauce <dtn3423@gmail.com>
2018-01-26 00:28:23 -0500

    initial commit and setup repo

---
+++ PersonalProjects/oly-medals-2018/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const SRC_DIR = path.resolve(__dirname, './app');
+const BUILD_DIR = path.resolve(__dirname, './app/static');
+
+module.exports = {
+  entry: path.resolve(SRC_DIR, 'Index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['es2015', 'react'] }
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  }
+}

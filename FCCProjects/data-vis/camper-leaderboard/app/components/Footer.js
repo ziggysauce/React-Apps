9915d9e93ce383b7a9bc0d015a55b588233d48ba
ziggysauce <dtn3423@gmail.com>
2018-02-06 01:21:18 -0500

    Initial setup

---
+++ FCCProjects/data-vis/camper-leaderboard/app/components/Footer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../styles/footer.scss';
+
+class Footer extends Component {
+  constructor() {
+		super();
+		this.state = {};
+	}
+	
+  render() {
+		return (
+			<footer>
+        <ul className="footer-links">
+          <li><a href="https://www.linkedin.com/in/daniel-nguyen-1a041495/" target="_blank"><i className="fab fa-linkedin social-icon"></i></a></li>
+          <li><a href="https://github.com/ziggysauce" target="_blank"><i className="fab fa-github social-icon"></i></a></li>
+          <li><a href="https://codepen.io/ziggysauce/" target="_blank"><i className="fab fa-codepen social-icon"></i></a></li>
+          <li><a href="https://medium.com/@ziggysauce" target="_blank"><i className="fab fa-medium social-icon"></i></a></li>
+        </ul>
+        <p>Created by <span>Dan Nguyen</span></p>
+      </footer>
+		);
+  }
+}
+
+export default Footer;

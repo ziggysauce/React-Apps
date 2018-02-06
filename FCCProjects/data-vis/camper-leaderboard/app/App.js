09a01111ac3d4043aa084cda36e4f515f9476b9e
ziggysauce <dtn3423@gmail.com>
2018-02-06 10:08:14 -0500

    Setup API calls

--- FCCProjects/data-vis/camper-leaderboard/app/App.js
+++ FCCProjects/data-vis/camper-leaderboard/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import styles from './styles/app.scss';
 import Leaderboard from './components/Leaderboard';
@@ -7,15 +8,47 @@ import Footer from './components/Footer';
 
 
 class App extends Component {
-  constructor() {
-		super();
-		this.state = {};
+  constructor(props) {
+		super(props);
+		this.state = {
+			data: [{}]
+		};
+	}
+
+	getRecent() {
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+		.then((results) => {
+			const recentData = results.data;
+			this.setState({
+				data: recentData
+			});
+		}).catch(err => {
+			console.log(err);
+		});
+	}
+
+	getAllTime() {
+		axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+		.then((results) => {
+			const recentData = results.data;
+			this.setState({
+				data: recentData
+			});
+		}).catch(err => {
+			console.log(err);
+		});
+	}
+
+	componentDidMount() {
+		this.getRecent();
 	}
 	
   render() {
+		console.log('initial data: ', this.state);
+
 		return (
 			<div className="app-container">
-				<Leaderboard />
+				<Leaderboard data={this.state.data} />
 				<Footer />
 			</div>
 		);
--- FCCProjects/data-vis/camper-leaderboard/app/components/Leaderboard.js
+++ FCCProjects/data-vis/camper-leaderboard/app/components/Leaderboard.js
@@ -3,70 +3,75 @@ import ReactDOM from 'react-dom';
 import styles from '../styles/leaderboard.scss';
 
 class Leaderboard extends Component {
-  constructor() {
-		super();
-		this.state = {};
-	}
-	
   render() {
+    console.log('hit: ', this.props.data[0].username);
+
 		return (
 			<div className="leaderboard-container">
-        <h1>camperLeaderboard <i className="fab fa-free-code-camp fa-lg"></i></h1>
+        <h1>camperLeaderboard  
+          <a href="https://www.freecodecamp.org" target="_blank">
+          <i className="fab fa-free-code-camp fa-lg fc-logo"></i>
+          </a>
+        </h1>
         <table>
-          <tr>
-            <th>Rank</th>
-            <th>Camper</th>
-            <th className="thirty-days">Points (past 30 days)</th>
-            <th className="all-time">Points (all time)</th>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
-          <tr>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-            <td>Filler</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Rank</th>
+              <th>Camper</th>
+              <th className="thirty-days">Points (past 30 days)</th>
+              <th className="all-time">Points (all time)</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>7</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+            <tr>
+              <td>8</td>
+              <td>Filler</td>
+              <td>Filler</td>
+              <td>Filler</td>
+            </tr>
+          </tbody>
         </table>
       </div>
 		);

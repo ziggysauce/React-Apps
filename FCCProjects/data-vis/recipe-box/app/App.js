f02954c0610a112e0ae0ea8cea513511ee27aa58
ziggysauce <dtn3423@gmail.com>
2018-02-24 12:48:21 -0500

    Fix localstorage to show seed data initially, then leave empty if user deletes everything

--- FCCProjects/data-vis/recipe-box/app/App.js
+++ FCCProjects/data-vis/recipe-box/app/App.js
@@ -8,30 +8,6 @@ import Add from './components/Add';
 import Form from './components/Form';
 import OptionModal from './components/OptionModal';
 
-// Seed data for recipes
-const recipes = [
-	{
-		image: 'https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=2700&q=80',
-		title: 'Dumplings',
-		ingredients: ['meat', 'wonton skin', 'vegetables']
-	},
-	{
-		image: 'https://images.unsplash.com/photo-1490091119006-59eb16fe739d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e6bd3a03c302aa8767030bec748888ca&auto=format&fit=crop&w=2689&q=80',
-		title: 'Avocado Toast',
-		ingredients: ['bread', 'avocado']
-	},
-	{
-		image: 'https://images.unsplash.com/photo-1428660386617-8d277e7deaf2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d85cfa1d6d003f6e82f46bc5a394a592&auto=format&fit=crop&w=1567&q=80',
-		title: 'Burgers',
-		ingredients: ['buns', 'patties', 'cheese', 'bacon', 'vegetables']
-	},
-	{
-		image: 'https://images.unsplash.com/photo-1512852939750-1305098529bf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a5832df503143f0eb527593cd0c5abe6&auto=format&fit=crop&w=1950&q=80',
-		title: 'Caesar Salad',
-		ingredients: ['lettuce', 'cheese', 'croutons', 'chicken', 'caesar dressing']
-	}
-];
-
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -114,18 +90,44 @@ class App extends Component {
 			const recipes = JSON.parse(json);
 
       if (recipes) {
+				console.log('local storage has recipes');
         this.setState((prevState) => ({
 					recipes: prevState.recipes.concat(recipes)
 				}));
-      }
-    } catch (e) {
-			// Do nothing at all
-			this.setState({ recipes: recipes });
-    }
+      } else {
+				console.log("local storage has no recipes");
+				// Seed data for recipes
+				this.setState({ 
+					recipes: 
+					[
+						{
+							image: 'https://images.unsplash.com/photo-1496116218417-1a781b1c416c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aba7663f8f57d37a53706edff54fd1c6&auto=format&fit=crop&w=2700&q=80',
+							title: 'Dumplings',
+							ingredients: ['meat', 'wonton skin', 'vegetables']
+						},
+						{
+							image: 'https://images.unsplash.com/photo-1490091119006-59eb16fe739d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e6bd3a03c302aa8767030bec748888ca&auto=format&fit=crop&w=2689&q=80',
+							title: 'Avocado Toast',
+							ingredients: ['bread', 'avocado']
+						},
+						{
+							image: 'https://images.unsplash.com/photo-1428660386617-8d277e7deaf2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d85cfa1d6d003f6e82f46bc5a394a592&auto=format&fit=crop&w=1567&q=80',
+							title: 'Burgers',
+							ingredients: ['buns', 'patties', 'cheese', 'bacon', 'vegetables']
+						},
+						{
+							image: 'https://images.unsplash.com/photo-1512852939750-1305098529bf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a5832df503143f0eb527593cd0c5abe6&auto=format&fit=crop&w=1950&q=80',
+							title: 'Caesar Salad',
+							ingredients: ['lettuce', 'cheese', 'croutons', 'chicken', 'caesar dressing']
+						}
+					] 
+				});
+			}
+    } catch (e) {}
 	}
 	
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.recipes !== this.state.recipes) {
+    if ((prevState.recipes.length !== this.state.recipes.length) || (prevState.recipes !== this.state.recipes)) {
       const json = JSON.stringify(this.state.recipes);
       localStorage.setItem('recipes', json);
     }
